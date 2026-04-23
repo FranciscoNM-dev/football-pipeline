1b82349ea749223c94a060624d354c6ba21b1dcf
Francisco Navarro <franciscoalfonsonm@gmail.com>
2026-04-23 14:27:57 +0200

    Update transform.py to include team column in df

--- src/transform.py
+++ src/transform.py
@@ -4,5 +4,5 @@ import pandas as pd
 def clean_shots(ugly_df:pd.DataFrame) -> pd.DataFrame:
     ugly_df = ugly_df.rename(columns={"shot_statsbomb_xg": "xg"})
     ugly_df = ugly_df.dropna(subset=['xg'])
-    return ugly_df[['match_id', 'player', 'minute', 'xg', 'shot_outcome']]
+    return ugly_df[['match_id', 'player', 'team', 'minute', 'xg', 'shot_outcome']]
 
