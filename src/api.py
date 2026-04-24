f112255d2fb391e24e1dd9379b4c654396aeb83a
Francisco Navarro <franciscoalfonsonm@gmail.com>
2026-04-24 20:12:05 +0200

    implemented number of simulations in api.py so mean of n simulations can be calculated

--- src/api.py
+++ src/api.py
@@ -24,7 +24,6 @@ def simulation_endpoint(match_id: int):
 #localhost/standings?simulations que es el param de abajo
 @app.get("/standings")
 def get_standings(simulations: int = 100):
-    #Probando a simular una liga solamente
     sql = sqlalchemy.text(
     "SELECT distinct match_id FROM shots"
     )
@@ -37,12 +36,15 @@ def get_standings(simulations: int = 100):
     base_table = {'team': teams, 'points': [0] * len(teams)}
     standings = pd.DataFrame(base_table)
 
-    for id in match_ids:
-        result = utils.simulate_match(id, engine)
-        points = utils.calculate_points(result)
-        for team in list(result.keys()):
-            standings.loc[standings.team == team, 'points'] += points[team]
+    for simulation in range(simulations):
+        for id in match_ids:
+            result = utils.simulate_match(id, engine)
+            points = utils.calculate_points(result)
+            for team in list(result.keys()):
+                standings.loc[standings.team == team, 'points'] += points[team]
     
+
+    standings['points'] = standings['points'].apply(lambda x: x / simulations)
     final_table = standings.sort_values(by = 'points', ascending = False)
     return final_table.to_dict(orient='records')
 
